49e46193f643777e988dfda208231905d3853c1b
LauraNicole449 <lauranicolegarcia449@gmail.com>
2025-01-21 17:30:39 +0100

    Agregar archivos de proyecto.

---
+++ GestionContactos/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace GestionContactos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.añadirContacto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // añadirContacto
+            // 
+            this.añadirContacto.Location = new System.Drawing.Point(269, 79);
+            this.añadirContacto.Name = "añadirContacto";
+            this.añadirContacto.Size = new System.Drawing.Size(96, 23);
+            this.añadirContacto.TabIndex = 0;
+            this.añadirContacto.Text = "Añadir contacto";
+            this.añadirContacto.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.añadirContacto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button añadirContacto;
+    }
+}
+
